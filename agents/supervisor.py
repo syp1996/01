6e2b5e1202659262e4da8df493f98fa326913278
Yunpeng Shi <y.shi27@newcastle.ac.uk>
2026-01-28 14:03:38 +0800

    修复uuid导入问题

--- agents/supervisor.py
+++ agents/supervisor.py
@@ -6,6 +6,8 @@ LastEditTime: 2026-01-28 10:04:17
 FilePath: /01/agents/supervisor.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+import uuid
+
 from langchain_core.messages import HumanMessage, SystemMessage
 
 from state import PlanningResponse, agentState
